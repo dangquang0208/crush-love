87dccbec9c28f6d5796febb02a315b23adb8b341
dangquang0208 <100954349+dangquang0208@users.noreply.github.com>
2022-03-04 23:07:56 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
   titleHeader: "Làm ơn Ngọc Duyên hãy hết giận bạn Quang này nhó",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+    "Duyên chỉ chọn được một trong hai thoi nên cứ thử de =))))) ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Cảm ơn mầy vì đồng ý",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý mà ^^ </br> Cám ơn cậu đã đồng ý để hết giận t nhó :)) </br> "Nãy t cũng rất buồn đó. </br> Nên m nên hết giận t ik "',
+    'Tớ biết là Duyên sẽ đồng ý mà ^^ </br> Cám ơn Duyên đã đồng ý để hết giận bạn Quang nhó :)) </br> "Nãy t cũng rất buồn đó. </br> Nên m nên hết giận t ik "',
 };
